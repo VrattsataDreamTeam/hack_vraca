458dd2d0f6d6a6052710c77ce229c49d23753447
cvexa <c_v_e_x_a@abv.bg>
2016-04-16 23:38:49 +0300

    bootstrap_map_button_and_more

--- delete.php
+++ delete.php
@@ -1,4 +1,5 @@
 <?php 
+include('functions/header.php');
 $conn = mysqli_connect('localhost', 'root', '', 'zones');
 // if (!$conn) {
 // 	die("Connection failed: " .mysqli_connect_error());
@@ -15,9 +16,9 @@ $delete_query = 	"UPDATE photos
 $delete_result = mysqli_query($conn, $delete_query);
 
 if ($delete_result) {
- 				
-		echo "Успешно изтрихте запис в базата данни!";
-		echo "<p><a href='read.php'>Read DB</a></p>";
+ 		echo "<div id='talon' class='bg-info col-md-4 col-md-offset-4 col-xs-10 col-xs-offset-1'>";		
+		echo "Успешно изтрихте снимката от базата данни!";
+		echo "<p><a href='read.php'><button class='btn-primary btn-lg btn-block'>Виж Снимките...</button></a></p>";
 	}else{
 		echo "Неуспешно изтриване на запис в базата данни! Моля опитайте по-късно!";
 		echo "<p><a href='#'>link to somewhere ... </a></p>";
--- map.php
+++ map.php
@@ -9,7 +9,14 @@ $zone_id=$_SESSION['zone_id'];
     <title>Simple Map</title>
     <meta name="viewport" content="initial-scale=1.0">
     <meta charset="utf-8">
-    
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     <style>
       html, body {
         height: 100%;
@@ -22,10 +29,11 @@ $zone_id=$_SESSION['zone_id'];
     </style>
   </head>
   <body>
-    <div id="map"></div>
     <?php 
     echo '<p><a href="free.php?zone_id='.$zone_id.'" class="btn btn-primary btn-default btn-block">Назад </a></p></div></div>';
     ?>
+    <div id="map"></div>
+    
     <script>
       var map;
       function initMap() {
